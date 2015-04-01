44688e322d77a8292549bfc8b0d16120f707ef63
terry <zwei@kalengo.com>
2015-04-01 16:45:55 +0800

    fixbug

--- test/nba_test.js
+++ test/nba_test.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
 var superagent = require('superagent');
 
-var host = 'localhost:3000';
+// var host = 'localhost:3000';
 
-var testurl = '/nba/getInfo1';
+// var testurl = '/nba/getInfo1';
 
 var getNbaData = function(url,callback){
     superagent.get(url).end(function(err,res){
@@ -15,7 +15,7 @@ describe('nba data', function(){
     this.timeout(50000);
     describe('#getNbaInfo1()', function(){
         it('get nba data info',function(done){
-            getNbaData('http://'+host+testurl,function(err,res){
+            getNbaData('http://stats.nba.com/stats/homepagev2?GameScope=Season&LeagueID=00&PlayerOrTeam=Player&PlayerScope=All+Players&Season=2014-15&SeasonType=Regular+Season&StatType=Traditional',function(err,res){
                 if(err) throw err;
                 done();
             });
